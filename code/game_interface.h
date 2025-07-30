7407bf6b6b120044ff8ce297c6eee4f818bb2d43
NoeRicklin <noe.ricklin@outlook.de>
2025-07-30 17:39:12 +0200

    Implemented functions to test win conditions and keep track of game stats

---
+++ code/game_interface.h
@@ -0,0 +1,36 @@
+#define FALSE		0
+#define TRUE		1
+
+// Board constants
+#define board_height	6
+#define board_width	7
+
+// Put Errors
+#define PUT_SUCCESS	0
+#define INVALID_COLUMN	1
+#define FULL_COLUMN	2
+#define INVALID_PLAYER	3
+
+// Win constants (Bit-activated)
+#define NOT_OVER	0b00000001
+#define WIN 		0b00000010
+#define DRAW		0b00000100
+
+#define VERTICAL_WIN	0b00001000
+#define HORIZONTAL_WIN	0b00010000
+#define DIAGONAL_WIN	0b00100000
+
+#define GAME_MOVE_OFFSET 8
+
+// Tile names
+#define PLAYER_ONE	1
+#define PLAYER_TWO	-1
+#define EMPTY_TILE	0
+
+int put_stone(int col, int player);
+void print_game_state();
+int test_win(int col, int player);
+
+int _test_hor_win(int row, int player);
+int _test_vert_win(int col, int player);
+int _test_diag_win(int row, int col, int player);
--- code/play.c
+++ code/play.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "game_interface.h"
+
 int main() {
-    return 0;
+    
 }
